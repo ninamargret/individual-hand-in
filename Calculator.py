d0646cc078a96fc36ec6641311833d4521848033
Nina M <nina19@ru.is>
2020-03-16 15:42:14 +0000

    Add method ignores numbers bigger than 1000

--- Calculator.py
+++ Calculator.py
@@ -1,18 +1,33 @@
+
+
 class Calculator:
+    
     @staticmethod
     def Add(input: str) -> int:
         if input == "":
             return 0
+        
         elif len(input) >= 3:
             num_list = input.split(",")
             sum = 0 
             for number_str in num_list:
                 if "\n" in number_str:
                     num1, num2 = number_str.split("\n")
-                    sum += int(num1) + int(num2)
+                    num1 = check_number_str(int(num1))
+                    num2 = check_number_str(int(num2))
+                    sum += num1 + num2
                 else:
-                    sum += int(number_str)
+                    number_str = check_number_str(int(number_str))
+                    sum += number_str
 
             return sum
+        
         else:
             return int(input)
+
+
+def check_number_str(number_str: int)-> int:
+        if number_str > 1000:
+            return 0
+        else:
+            return number_str
--- test_Calculator.py
+++ test_Calculator.py
@@ -1,23 +1,25 @@
 from Calculator import Calculator
 
-def test_add_empty_returns_zero()-> None:
+def test_add_empty_returns_zero() -> None:
     assert Calculator.Add("") == 0
 
-def test_add_number_returns_number()-> None:
+def test_add_number_returns_number() -> None:
     assert Calculator.Add("1") == 1
 
-def test_add_two_numbers_returns_sum_of_numbers()-> None:
+def test_add_two_numbers_returns_sum_of_numbers() -> None:
     assert Calculator.Add("1,2") == 3
 
-def test_add_unknown_numbers_returns_sum_of_numbers()-> None:
+def test_add_unknown_numbers_returns_sum_of_numbers() -> None:
     assert Calculator.Add("1,2,3,4,5") == 15
 
-def test_add_unknown_numbers_returns_sum_of_numbers_vol2()-> None:
+def test_add_unknown_numbers_returns_sum_of_numbers_vol2() -> None:
     assert Calculator.Add("10,2,5,22,1,1") == 41
 
-def test_add_unknown_numbers_returns_sum_of_numbers_vol3()-> None:
+def test_add_unknown_numbers_returns_sum_of_numbers_vol3() -> None:
     assert Calculator.Add("10,2,5,22,1,1,105,342") == 488
 
 def test_add_new_line_returns_sum_of_numbers() -> None:
     assert Calculator.Add("1\n2,3") == 6
 
+def test_add_ignore_numbers_bigger_than_1000() -> None:
+    assert Calculator.Add("1001,2") == 2
